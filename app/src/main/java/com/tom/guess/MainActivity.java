113eda10e9a66dcada728168dacc3eb67c6605f8
miia <miia@com.tw>
2019-10-29 16:28:54 +0800

    1029

--- app/src/main/java/com/tom/guess/MainActivity.java
+++ app/src/main/java/com/tom/guess/MainActivity.java
@@ -1,10 +1,12 @@
 package com.tom.guess;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,8 +23,10 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     String TAG = MainActivity.class.getSimpleName();
-    private TextView number;
+    private EditText number;
     private int num;
+    int counter;
+    private TextView edCounter;
     int secret = new Random().nextInt(10)+1;
 
 
@@ -29,12 +34,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "seceret " + secret);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         number = findViewById(R.id.number);
-       num  = number.getText().length();
+        edCounter = findViewById(R.id.counter);
+        edCounter.setText(counter+ "");
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                    reset();
+                }
+            });
+        reset();
+        num  = number.getText().length();
+        Log.d(TAG, "seceret " + secret);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,16 +60,53 @@ public class MainActivity extends AppCompatActivity {
     }
     public void guess(View view){
         int num =Integer.parseInt(number.getText().toString());
+        counter++;
+        edCounter.setText(counter+"");
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                reset();
+            }
+        };
         number.setText(Integer.toString(num));
+        String message = "So good!";
         if (num < secret){
-            Toast.makeText(MainActivity.this, "Higher", Toast.LENGTH_SHORT).show();
-        }else if (num>secret){
-            Toast.makeText(MainActivity.this, "Lower", Toast.LENGTH_SHORT).show();
-        }else {
-            Toast.makeText(MainActivity.this, "you are right!", Toast.LENGTH_SHORT).show();
+            message = "Bigger";
+            listener = null;
+            //Toast.makeText(MainActivity.this, "Higher", Toast.LENGTH_SHORT).show();
+           /* new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("hahaha")
+                    .setMessage("bigger")
+                    .setPositiveButton("OK",null)
+                    .show();*/
+
+        } else if (num > secret){
+            message = "Smaller";
+            listener = null;
+            //Toast.makeText(MainActivity.this, "Lower", Toast.LENGTH_SHORT).show();
+            /*new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("hahaha")
+                    .setMessage("lower")
+                    .setPositiveButton("OK",null)
+                    .show();*/
+
+        } else {
+            //Toast.makeText(MainActivity.this, "you are right!", Toast.LENGTH_SHORT).show();
+
         }
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage("You are right!")
+                .setPositiveButton("OK",null)
+                .show();
 
     }
+
+    private void reset() {
+        secret = new Random().nextInt(10)+1;
+        counter=0;
+        edCounter.setText(counter+"");
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
